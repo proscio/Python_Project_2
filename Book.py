5524cecd210d9eddbd55995601bcc41c2b263f66
Patrick Roscio <116911676+proscio@users.noreply.github.com>
2024-05-05 20:31:23 -0400

    These were never private variables??

--- Book.py
+++ Book.py
@@ -1,9 +1,6 @@
 from Media import Media
 
 class Book(Media):
-     """
-    The Book class inherits from the Media class and represents a book in the media library.
-    """
     def __init__(self, id, title, averageRating, authors, isbn, isbn13, languageCode, pageCount, numRatings, publicationDate, publisher):
         super().__init__(id, title, averageRating)
         self.__authors = authors
@@ -16,74 +13,25 @@ class Book(Media):
         self.__publisher = publisher
 
     def getAuthors(self):
-         """
-        Get the authors of the book.
-        
-        :return: The authors of the book.
-        :rtype: str
-        """
         return self.__authors
 
     def getIsbn(self):
-        """
-        Get the ISBN of the book.
-        
-        :return: The ISBN of the book.
-        :rtype: str
-        """
         return self.__isbn
 
     def getIsbn13(self):
-        """
-        Get the ISBN-13 of the book.
-        
-        :return: The ISBN-13 of the book.
-        :rtype: str
-        """
         return self.__isbn13
 
     def getLanguageCode(self):
-        """
-        Get the language code of the book.
-        
-        :return: The language code of the book.
-        :rtype: str
-        """
         return self.__languageCode
 
     def getPageCount(self):
-        """
-        Get the number of pages in the book.
-        
-        :return: The number of pages in the book.
-        :rtype: int
-        """
         return self.__pageCount
 
     def getNumRatings(self):
-        """
-        Get the number of ratings the book has received.
-        
-        :return: The number of ratings the book has received.
-        :rtype: int
-        """
         return self.__numRatings
 
     def getPublicationDate(self):
-        """
-        Get the publication date of the book.
-        
-        :return: The publication date of the book.
-        :rtype: str
-        """
-
         return self.__publicationDate
 
     def getPublisher(self):
-        """
-        Get the publisher of the book.
-        
-        :return: The publisher of the book.
-        :rtype: str
-        """
         return self.__publisher
--- Show.py
+++ Show.py
@@ -1,239 +1,45 @@
 from Media import Media
 
 class Show(Media):
-    """
-    The Show class inherits from the Media class and represents a TV show from the media csv file.
-    """
     def __init__(self, id, title, averageRating, typeShow, directors, actors, countryCode, dateAired, releaseYear, rating, duration, genres, description):
-        """
-        Initialize a Show object.
-        
-        :param id: The unique identifier for the show.
-        :type id: str
-        :param title: The title of the show.
-        :type title: str
-        :param averageRating: The average rating of the show.
-        :type averageRating: float
-        :param typeShow: The type of the show (e.g., Movie, TV Show).
-        :type typeShow: str
-        :param directors: The directors of the show.
-        :type directors: str
-        :param actors: The actors in the show.
-        :type actors: str
-        :param countryCode: The country code of the show.
-        :type countryCode: str
-        :param dateAired: The date the show aired.
-        :type dateAired: str
-        :param releaseYear: The release year of the show.
-        :type releaseYear: int
-        :param rating: The rating of the show.
-        :type rating: str
-        :param duration: The duration of the show.
-        :type duration: str
-        :param genres: The genres of the show.
-        :type genres: str
-        :param description: The description of the show.
-        :type description: str
-        """
         super().__init__(id, title, averageRating)
-        self.typeShow = typeShow
-        self.directors = directors
-        self.actors = actors
-        self.countryCode = countryCode
-        self.dateAired = dateAired
-        self.releaseYear = releaseYear
-        self.rating = rating
-        self.duration = duration
-        self.genres = genres
-        self.description = description
+        self.__typeShow = typeShow
+        self.__directors = directors
+        self.__actors = actors
+        self.__countryCode = countryCode
+        self.__dateAired = dateAired
+        self.__releaseYear = releaseYear
+        self.__rating = rating
+        self.__duration = duration
+        self.__genres = genres
+        self.__description = description
 
     def getTypeShow(self):
-        """
-        Get the type of the show.
-        
-        :return: The type of the show.
-        :rtype: str
-        """
-        return self.typeShow
+        return self.__typeShow
 
     def getDirectors(self):
-        """
-        Get the directors of the show.
-        
-        :return: The directors of the show.
-        :rtype: str
-        """
-        return self.directors
+        return self.__directors
 
     def getActors(self):
-        """
-        Get the actors in the show.
-        
-        :return: The actors in the show.
-        :rtype: str
-        """
-        return self.actors
+        return self.__actors
 
     def getCountryCode(self):
-        """
-        Get the country code of the show.
-        
-        :return: The country code of the show.
-        :rtype: str
-        """
-        return self.countryCode
+        return self.__countryCode
 
     def getDateAired(self):
-        """
-        Get the date the show aired.
-        
-        :return: The date the show aired.
-        :rtype: str
-        """
-        return self.dateAired
+        return self.__dateAired
 
     def getReleaseYear(self):
-        """
-        Get the release year of the show.
-        
-        :return: The release year of the show.
-        :rtype: int
-        """
-        return self.releaseYear
+        return self.__releaseYear
 
     def getRating(self):
-        """
-        Get the rating of the show.
-        
-        :return: The rating of the show.
-        :rtype: str
-        """
-        return self.rating
+        return self.__rating
 
     def getDuration(self):
-        """
-        Get the duration of the show.
-        
-        :return: The duration of the show.
-        :rtype: str
-        """
-        return self.duration
+        return self.__duration
 
     def getGenres(self):
-        """
-        Get the genres of the show.
-        
-        :return: The genres of the show.
-        :rtype: str
-        """
-        return self.genres
+        return self.__genres
 
     def getDescription(self):
-        """
-        Get the description of the show.
-        
-        :return: The description of the show.
-        :rtype: str
-        """
-        return self.description
-
-    def setTypeShow(self, typeShow):
-        """
-        Set the type of the show.
-        
-        :param typeShow: The new type for the show.
-        :type typeShow: str
-        :return: None
-        """
-        self.typeShow = typeShow
-
-    def setDirectors(self, directors):
-        """
-        Set the directors of the show.
-        
-        :param directors: The new directors for the show.
-        :type directors: str
-        :return: None
-        """
-        self.directors = directors
-
-    def setActors(self, actors):
-        """
-        Set the actors in the show.
-        
-        :param actors: The new actors for the show.
-        :type actors: str
-        :return: None
-        """
-        self.actors = actors
-
-    def setCountryCode(self, countryCode):
-        """
-        Set the country code of the show.
-        
-        :param countryCode: The new country code for the show.
-        :type countryCode: str
-        :return: None
-        """
-        self.countryCode = countryCode
-
-    def setDateAired(self, dateAired):
-        """
-        Set the date the show aired.
-        
-        :param dateAired: The new date the show aired.
-        :type dateAired: str
-        :return: None
-        """
-        self.dateAired = dateAired
-
-    def setReleaseYear(self, releaseYear):
-        """
-        Set the release year of the show.
-        
-        :param releaseYear: The new release year for the show.
-        :type releaseYear: int
-        :return: None
-        """
-        self.releaseYear = releaseYear
-
-    def setRating(self, rating):
-        """
-        Set the rating of the show.
-        
-        :param rating: The new rating for the show.
-        :type rating: str
-        :return: None
-        """
-        self.rating = rating
-
-    def setDuration(self, duration):
-        """
-        Set the duration of the show.
-        
-        :param duration: The new duration for the show.
-        :type duration: str
-        :return: None
-        """
-        self.duration = duration
-
-    def setGenres(self, genres):
-        """
-        Set the genres of the show.
-        
-        :param genres: The new genres for the show.
-        :type genres: str
-        :return: None
-        """
-        self.genres = genres
-
-    def setDescription(self, description):
-        """
-        Set the description of the show.
-        
-        :param description: The new description for the show.
-        :type description: str
-        :return: None
-        """
-        self.description = description
-        
+        return self.__description   
